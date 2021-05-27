2c6eb66420cbf4e4c5dded94295832a4b0fc09be
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2021-05-27 17:03:03 +0100

    Initial (Rough) and in test v1 Saba Cloud API)
    Version 1 of Saba Cloud API (username and password, Certificate retrieval, resuse and an example call to get example data)

---
+++ Saba_Integration1.py
@@ -0,0 +1,57 @@
+import requests
+from requests.exceptions import HTTPError
+
+payload = ""
+headers = {
+    'Content-Type': 'application/json',
+    'user': 'Your_username',
+    'password': 'Your_Password'
+}
+try:
+    response = requests.get("https://logrhythmsb-api.sabacloud.com/v1/login", headers=headers, data=payload)
+    response.raise_for_status()
+    # access Json content
+    jsonResponse = response.json()
+
+    print("Entire JSON response")
+    print(jsonResponse)
+    print("Print each key-value pair from JSON response")
+    for key, value in jsonResponse.items():
+        print(key, ":", value)
+    print("Certificate is ")
+    print(jsonResponse['certificate'])
+    sabacertificate = (jsonResponse['certificate'])
+    print("Dict item for sabacertificate is")
+    print(sabacertificate)
+
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
+
+# Class Retrieval post certificate
+
+params = {
+    'type': 'web-based',
+    'count': '10',
+    'startPage': '1',
+    'SabaCertificate': '' + str(sabacertificate)
+}
+headers = {
+    'Content-Type': 'application/json'
+}
+try:
+    r = requests.get('https://logrhythmsb-api.sabacloud.com/v1/offering', params=params)
+    # access Json content
+    print(r.url)
+    jsonResponseclass = r.json()
+    print("Entire JSON Class response")
+    print(jsonResponseclass)
+    print("Print each key-value pair from JSON response for classes")
+    for key, value in jsonResponseclass.items():
+        print(key, ":", value)
+
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
